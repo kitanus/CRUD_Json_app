a56d124c585b04237f814ae6ef17197bd09cfb5b
kitanus <kulukafarmin@mail.ru>
2020-03-14 15:50:51 +0300

    Added alerts and update data show

--- app.js
+++ app.js
@@ -8,24 +8,29 @@ const fs = require('fs');
 const app = express();
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 
-let id = false;
+let update = [];
+update["id"] = false;
+let msg = [];
 let jsonName = null;
 
 app.use(express.static(__dirname));
 app.set('views engine', 'pug');
 
-app.get("/", function(req, res)
+app.get("/", urlencodedParser, function(req, res)
 {
     if(jsonName !== null)
     {
         let obj = JSON.parse(fs.readFileSync(jsonName+".json", 'utf8'));
-        res.render('index', {id: id, items: obj["Hospitals"]});
+        res.render('index', {
+            update: update,
+            items: obj["Hospitals"],
+            msg: msg
+        });
     }
     else
     {
-        res.render('index', {id: false, items: false});
+        res.render('index', {update: false, items: false, msg: msg});
     }
-
 });
 
 app.post("/json", urlencodedParser, function(req, res)
@@ -33,11 +38,13 @@ app.post("/json", urlencodedParser, function(req, res)
     fs.access(req.body.json+".json", function(error){
         if (error)
         {
+            msg["json"] = "Ошибка! Не правильно введено название json файла ";
             res.redirect("/");
         }
         else
         {
             jsonName = req.body.json;
+            msg["json"] = "";
             res.redirect("/");
         }
     });
@@ -49,10 +56,12 @@ app.post("/create", urlencodedParser, function(req, res)
 
     if (matches === null)
     {
+        msg["phone"] = "Ошибка! Неправильно введен телефон";
         res.redirect("/");
     }
     else
     {
+        msg["phone"] = "";
         let obj = JSON.parse(fs.readFileSync(jsonName+".json", 'utf8'));
         obj["Hospitals"].push(req.body);
         let json = JSON.stringify(obj);
@@ -69,23 +78,30 @@ app.post("/update", urlencodedParser, function(req, res)
 
     if (matches === null)
     {
+        msg["phone"] = "Ошибка! Неправильно введен телефон";
         res.redirect("/");
     }
-    else {
+    else
+    {
+        msg["phone"] = "";
         let obj = JSON.parse(fs.readFileSync('lpu.json', 'utf8'));
         obj["Hospitals"][req.body.id]["full_name"] = req.body.full_name;
         obj["Hospitals"][req.body.id]["address"] = req.body.address;
         obj["Hospitals"][req.body.id]["phone"] = req.body.phone;
         let json = JSON.stringify(obj);
         fs.writeFileSync(jsonName + ".json", json);
-        id = false;
+        update = false;
         res.redirect("/");
     }
 });
 
 app.get("/update", urlencodedParser, function(req, res)
 {
-    id = req.query.id;
+    let obj = JSON.parse(fs.readFileSync(jsonName+".json", 'utf8'));
+    update['id'] = req.query.id;
+    update['full_name'] = obj["Hospitals"][req.query.id]['full_name'];
+    update['address'] = obj["Hospitals"][req.query.id]['address'];
+    update['phone'] = obj["Hospitals"][req.query.id]["phone"];
     res.redirect("/");
 });
 
